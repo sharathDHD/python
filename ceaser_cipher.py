80b06c025c65ba46def1b67c016f8bd29eee66bd
sharath kumar <nikelkumar55@gmail.com>
2018-12-16 18:22:00 +0530

    Update ceaser_cipher.py

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -16,15 +16,15 @@ def ceaser_decrypt( cipher_text , key = 3 , wspace = True , character_set = 127 
 		[ plain_text.append( chr(( abs( ord( x ) - key ) % character_set )))  for x in cipher_text ]	
 	return "".join(plain_text)
 
-if __name__ == 'main':
+if __name__ == '__main__':
 	while(choice == 0):
 		print(" 1 . Encrypt \n 2 . Decrypt\n 3.Exit")
 		choice = int( input( "Enter choice:" ))
 		if choice == 1 :
-			cipher_text = ceaser_ecnrypt( raw_input( " Enter plain Text " ) , int( intput( "Enter key:" )) , 1 != abs( int( input( "Enter 1 to encrypt along with spaces" )))
+			cipher_text = ceaser_ecnrypt( raw_input( " Enter plain Text " ) , int( intput( "Enter key:" )) , 1 != abs( int( input( "Enter 1 to encrypt along with spaces" ))))
 			print( cipher_text )
 		elif choice == 2 :					     
-			cipher_text = ceaser_ecnrypt( raw_input( " Enter plain Text " ) , int( intput( "Enter key:" )) , 1 != abs( int( input( "Enter 1 to encrypt along with spaces " )))
+			cipher_text = ceaser_ecnrypt( raw_input( " Enter plain Text " ) , int( intput( "Enter key:" )) , 1 != abs( int( input( "Enter 1 to encrypt along with spaces " ))))
 			print( cipher_text )
 		elif choice == 3 :
 			exit(0)
