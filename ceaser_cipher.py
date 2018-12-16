6521027981f150c7acee3d27857430b89c4009fd
sharath kumar <nikelkumar55@gmail.com>
2018-12-16 17:49:46 +0530

    Update ceaser_cipher.py

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -7,3 +7,12 @@ def ceaser_encrypt( plain_text , key = 3 , wspace = True ,character_set = 127):
 		[ cipher.append( chr(abs( ord( x ) + key ) % character_set )) for x in list( plain_text ) ]
 
 	return "".join( cipher )
+
+def ceaser_decrypt( cipher_text , key = 3 , wspace = True , character_set = 127 ):
+	plain_text = []
+	if wspace:
+		[ plain_text.append( chr(( abs( ord( x ) - key ) % character_set ))) if ord(x) != 32 else plain_text.append(chr(32)) for x in cipher_text ]
+	else:
+		[ plain_text.append( chr(( abs( ord( x ) - key ) % character_set )))  for x in cipher_text ]	
+	return "".join(plain_text)
+
